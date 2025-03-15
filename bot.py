d345d73c2f6f31862ae211d191c8c788c3b4b280
Hack337 <69693943+y9hack337@users.noreply.github.com>
2025-03-15 08:04:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -308,8 +308,7 @@ async def terminal_command(client, message):
         )
         processes[str(message.id)] = process
         await message.edit_text(
-            f"""⌨️
-<b> Системная команда: </b>
+            f"""⌨️<b> Системная команда: </b>
 <code>{command}</code>\n
 <b>Процесс ID: </b>
 <code>{process.pid}</code>""",
@@ -318,23 +317,20 @@ async def terminal_command(client, message):
         result = stdout.decode('utf-8').strip()
         error = stderr.decode('utf-8').strip()
         return_code = process.returncode
-        output = f"""📼
-<b> Вывод:</b>\n
+        output = f"""📼<b> Вывод:</b>\n
 <code>{result if result else 'Нет вывода'}</code>"""
         if error:
             output += f"""\n🚫
 <b> Ошибка:</b>\n
 <code>{error}</code>"""
         await message.edit_text(
-            f"""⌨️
-<b> Системная команда: </b>
+            f"""⌨️<b> Системная команда: </b>
 <code>{command}</code>\n
 <b>Код выхода: </b>
 <code>{return_code}</code>\n{output}""",
             parse_mode=ParseMode.HTML)
     except Exception as e:
-        await message.edit_text(f"""
-<b>Ошибка: </b>
+        await message.edit_text(f"""🚫<b>Ошибка: </b>
 <code>{e}</code>""", parse_mode=ParseMode.HTML)
     try:
         processes.pop(str(message.id))
